09b17d6196ffe1789fb4078c173248c373a35c5c
Aadithya-J <jlaadithya@gmail.com>
2024-06-24 17:55:37 +0530

    bug fix ,now transition only once

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -29,6 +29,7 @@ const Technologies = () => {
       <motion.h2 
         whileInView= {{opacity: 1, y: 0}}
         initial= {{opacity: 0,y: -80}}
+        viewport={{ once: true }}
         transition= {{duration: 0.5}}
         className="my-20 text-center text-4xl">
           Technologies
